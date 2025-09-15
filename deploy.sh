6912cc94697334a7572e112e8eafd7d3c7b3c9e1
andresr27 <andresrenaud@gmail.com>
2025-09-15 09:06:11 -0300

    Minor changes to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,11 @@ docker build -t ${IMAGE_URL}:stable .
 # if prod docker push
 popd || exit
 
-pwd
+# Deploy to k8s
 pushd kubernetes/dev || exit
-kubectl apply -f .
+# We want to set the new image and roll out a restart or simply wait K8s does this for us
+# If deploy not running apply all else just canary. Actions should handle the promotion with cli.sh?
+kubectl apply -f
 popd || exit
-
-
-
 
 echo "Deployment for ${IMAGE_URL} in progress..."
